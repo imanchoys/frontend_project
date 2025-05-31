80cfe3627c5a3b0d69e897d666a703a01aedd715
Krayushkina Anna 4216 <anna_ufa2004@mail.ru>
2025-05-31 17:48:56 +0300

    Fix index.js

--- smart_city/src/index.js
+++ smart_city/src/index.js
@@ -1,11 +1,17 @@
+// src/index.js
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client'; // Обратите внимание: импортируем из 'react-dom/client'
 import App from './App';
 import './styles.css';
 
-ReactDOM.render(
+const container = document.getElementById('root');
+
+// Создаём корень с помощью createRoot
+const root = ReactDOM.createRoot(container);
+
+// Рендерим наше приложение
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
